17294f5f089194b23b89984d2521384c667256b6
Ken Hibino <ken.hibino7@gmail.com>
2016-07-31 17:19:56 -0700

    Allow user to signin with either email or username

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -1,10 +1,15 @@
 class Users::SessionsController < ApplicationController
   def create
-    user = User.find_by(email: params[:email])
-    if user && user.authenticate(params[:password])
+    if user = User.authenticate(email_or_username, params[:password])
       render json: user, serializer: CurrentUserSerializer, status: 200
     else
       render json: { errors: ['Invalid email and password'] }, status: 422
     end
   end
+
+  private
+
+    def email_or_username
+      params[:email] || params[:username]
+    end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,9 @@ class User < ApplicationRecord
   validates :email, presence: true, uniqueness: { case_sensitive: false },
                     format: { with: EMAIL_REGEX }
   validates :username, presence: true, uniqueness: { case_sensitive: false }
+
+  def self.authenticate(email_or_username, password)
+    user = User.find_by(email: email_or_username) || User.find_by(username: email_or_username)
+    user && user.authenticate(password)
+  end
 end
