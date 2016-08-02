3ad464601318a33594c81959f7f87d94271f4b3d
Ken Hibino <ken.hibino7@gmail.com>
2016-08-01 22:56:28 -0700

    Add Carrierwave cofig for S3

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,2 +1,14 @@
 require 'carrierwave/orm/activerecord'
 
+if Rails.env.production?
+  CarrierWave.configure do |config|
+    config.fog_credentials = {
+      :provider               => 'AWS',
+      :aws_access_key_id      => ENV['S3_ACCESS_KEY'],
+      :aws_secret_access_key  => ENV['S3_SECRET_KEY'],
+      :region                 => ENV['S3_REGION']
+    }
+    config.fog_directory = ENV['S3_BUCKET']
+  end
+end
+
