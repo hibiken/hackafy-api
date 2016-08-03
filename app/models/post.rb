1a22bd1906c33a7ac7cfb00751bc52ae3eb08103
Ken Hibino <ken.hibino7@gmail.com>
2016-08-02 23:19:31 -0700

    Fix syntax error

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,7 +2,7 @@ class Post < ApplicationRecord
   belongs_to :user
 
   validates :user_id, presence: true
-  validates :photo presence: true
+  validates :photo, presence: true
 
   mount_uploader :photo, PhotoUploader
 end
