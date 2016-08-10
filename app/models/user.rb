589a82f3cad4a345a3574c7f2e6ae2581bdbfe2b
Ken Hibino <ken.hibino7@gmail.com>
2016-08-09 23:35:38 -0700

    Create relationship model and add follow/unfollow methods to User

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,15 @@ class User < ApplicationRecord
   has_many :likes, dependent: :destroy
   has_many :liked_posts, through: :likes, source: :post
   has_many :comments, dependent: :destroy
+  has_many :active_relationships, class_name: "Relationship",
+            foreign_key: "follower_id",
+            dependent: :destroy
+  has_many :following, through: :active_relationships, source: :followed
+
+  has_many :passive_relationships, class_name: "Relationship",
+            foreign_key: "followed_id",
+            dependent: :destroy
+  has_many :followers, through: :passive_relationships, source: :follower
 
   has_secure_password
   EMAIL_REGEX = /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
@@ -23,5 +32,18 @@ class User < ApplicationRecord
 
   def dislike!(post)
     likes.where(post_id: post.id).destroy_all
+  end
+
+  def follow(other_user)
+    return false if self.id == other_user.id
+    active_relationships.create(followed_id: other_user.id)
+  end
+
+  def unfollow(other_user)
+    active_relationships.find_by(followed_id: other_user.id).destroy
+  end
+
+  def following?(other_user)
+    following_ids.include?(other_user.id)
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160808022612) do
+ActiveRecord::Schema.define(version: 20160810060730) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -48,6 +48,15 @@ ActiveRecord::Schema.define(version: 20160808022612) do
     t.integer  "likes_count",    default: 0
     t.integer  "comments_count", default: 0
     t.index ["user_id"], name: "index_posts_on_user_id", using: :btree
+  end
+
+  create_table "relationships", force: :cascade do |t|
+    t.integer  "follower_id"
+    t.integer  "followed_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.index ["followed_id"], name: "index_relationships_on_followed_id", using: :btree
+    t.index ["follower_id"], name: "index_relationships_on_follower_id", using: :btree
   end
 
   create_table "users", force: :cascade do |t|
