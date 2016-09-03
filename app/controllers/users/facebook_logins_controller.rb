5e7de72b311aaef0cde3b9426191f29df491f41e
Ken Hibino <ken.hibino7@gmail.com>
2016-09-02 23:38:31 -0400

    Add a minor fix in facebook_logins#create action

--- app/controllers/users/facebook_logins_controller.rb
+++ app/controllers/users/facebook_logins_controller.rb
@@ -1,19 +1,19 @@
 class Users::FacebookLoginsController < ApplicationController
   def create
     # TODO: find by either facebook_id OR email
-    @user = User.find_by(facebook_id: params[:facebook_id])
+    @user = User.find_by(facebook_id: params[:facebook_id]) if params[:facebook_id].present?
     if @user
       render json: @user, serializer: CurrentUserSerializer, status: 200
     else
-      # TODO: create unique username
-      @new_user = User.create!(user_params)
-      render json: @new_user, serializer: CurrentUserSerializer, status: 201
+      @user = User.create!(user_params)
+      render json: @user, serializer: CurrentUserSerializer, status: 201
     end
   end
 
   private
 
     def user_params
+      # TODO: create unique username
       # TODO: generate random password
       params.permit(:facebook_id, :username).merge(password: 'password')
     end
