c0ff8595225959a3fa987d11e0292ebcc036a334
Ken Hibino <ken.hibino7@gmail.com>
2016-10-09 12:23:06 -0400

    Add metadata attribute to the output of NotificationSerializer

--- app/serializers/notification_serializer.rb
+++ app/serializers/notification_serializer.rb
@@ -1,10 +1,10 @@
 class NotificationSerializer < ActiveModel::Serializer
   attributes :id, :actor, :action_type, :read_at, :notifiable_id, :notifiable_type,
-             :photoUrl
+             :photoUrl, :metadata
 
   def actor
     {
-      'username' => object.actor.username, 
+      'username' => object.actor.username,
       'id' => object.actor_id,
       'avatarUrl' => object.actor.avatar_url
     }
@@ -15,4 +15,18 @@ class NotificationSerializer < ActiveModel::Serializer
       object.notifiable.photo.url
     end
   end
+
+  def metadata
+    case object.action_type
+    when 'LIKE_POST'
+      like_post_meatadata
+    end
+  end
+
+  private
+    def like_post_meatadata
+      {
+        'likesCount' => object.notifiable.reload.likes_count
+      }
+    end
 end
